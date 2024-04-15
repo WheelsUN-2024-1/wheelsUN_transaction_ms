56261f681ae491158b831cf5a40e3d771db72617
Cristian <cquilaguy@unal.edu.co>
2024-04-15 11:58:03 -0500

    UserdID int as DNINumber

--- database/CreditCardDao.go
+++ database/CreditCardDao.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type CreditCardDao struct {
 	gorm.Model
 	CreditCardId   uint   `json:"creditCardId"`
-	UserId         string    `json:"userId"`
+	UserId         int    `json:"userId"`
 	Number         string `json:"number"`
 	Name           string `json:"name"`
 	SecurityCode   string `json:"securityCode"`
